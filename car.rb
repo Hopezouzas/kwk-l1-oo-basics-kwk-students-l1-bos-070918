abd50b438bee031b28fff0a367757418d3380610
Hope Zouzas <hmzouzas@gmail.com>
2018-07-13 19:13:17 +0000

    automatically backed up by learn

--- car.rb
+++ car.rb
@@ -1,10 +1,19 @@
 class Car 
   attr_reader :model , :make , :year
   attr_accessor :heating
+  @@car_count = 0 
+  
+  
+  
   def initialize(model, make, year)
     @model = model 
     @make = make 
     @year = year 
+    @@car_count += 1 
+    
+  end 
+  def self.count 
+    @@car_count 
   end 
 end 
 
